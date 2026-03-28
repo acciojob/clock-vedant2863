6de5cf3467f0ad0edcd56f5cf0bdb4f1ed4768bb
Vedant Jadhav <121371485+vedant2863@users.noreply.github.com>
2026-03-28 14:43:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function updateTime() {
+    const now = new Date();
+
+    // format date & time
+    const currentTime = now.toLocaleString();
+
+    document.getElementById("timer").textContent = currentTime;
+}
+
+// update immediately
+updateTime();
+
+// update every second
+setInterval(updateTime, 1000);
